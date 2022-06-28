e09cfaca32d8112e811d378f64c67e2b62c5f39e
Maxime <diasmaxime1@gmail.com>
2022-06-28 16:42:22 +0200

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
       'access-control-allow-headers',
       'access-control-allow-origin',
       'content-type',
+      'authorization',
     ],
     origin: [
       'http://localhost:3000',
